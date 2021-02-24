6f79aa6a02b23be0ed6eb326d28deb27767028db
Stacie Taitague <stacie.taitague@gmail.com>
2021-02-24 15:18:45 -0500

    adds products on seeds.rb and in console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create(name: "jeans", price: 120, image_url: "http://www.jeans.com", description: "distressed premium denim")
+
+products = Product.create(name: "sunglasses", price: 120, image_url: "http://www.sunglasses.com", description: "classic aviators")
