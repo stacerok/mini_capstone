ee08af50d85d7c39f9ccd90c795959b5e0574576
Stacie Taitague <stacie.taitague@gmail.com>
2021-02-24 15:29:02 -0500

    adds JSON to display all data

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,7 @@
 class Api::ProductsController < ApplicationController
+  def products_actions
+    @products = Product.all
+    render "all_products.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/products_path" => "products#products_actions"
+  end
 end
